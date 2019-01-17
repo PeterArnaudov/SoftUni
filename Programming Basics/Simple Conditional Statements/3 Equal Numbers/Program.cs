90b0cf9b57329d5186d08cf9a19752117c9b0e4d
PeterArnaudov <45295322+PeterArnaudov@users.noreply.github.com>
2019-01-17 18:02:44 +0200

    Add Programming Basics

---
+++ Programming Basics/Simple Conditional Statements/3 Equal Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _3_Equal_Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            double number1 = double.Parse(Console.ReadLine());
+            double number2 = double.Parse(Console.ReadLine());
+            double number3 = double.Parse(Console.ReadLine());
+            string result;
+
+            if (number1 == number2 && number1 == number3 && number2 == number3)
+                result = "yes";
+            else
+                result = "no";
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Programming Basics/Simple Conditional Statements/Area of Figures/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Area_of_Figures
+{
+    class Program
+    {
+        static void Main()
+        {
+            string figureType = Console.ReadLine();
+            double area = 0;
+
+            if (figureType == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                area = side * side;
+            }
+            else if (figureType == "rectangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB = double.Parse(Console.ReadLine());
+                area = sideA * sideB;
+            }
+            else if (figureType == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                area = Math.PI * (radius * radius);
+            }
+            else if (figureType == "triangle")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                area = (side * height) / 2;
+            }
+
+            Console.WriteLine(Math.Round(area, 3));
+        }
+    }
+}
