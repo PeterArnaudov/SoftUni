82c351a50573a1da04a5b6c97b3df417cb6e9b3c
PeterArnaudov <45295322+PeterArnaudov@users.noreply.github.com>
2019-01-17 20:28:56 +0200

    Add Solutions

---
+++ Technology Fundamentals/Strings and Text Processing- Lab/Count Substring Occurrences/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_Substring_Occurrences
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string text = Console.ReadLine().ToLower();
+            string find = Console.ReadLine().ToLower();
+
+            int occurences = 0;
+            int index = text.IndexOf(find);
+
+            while (index != -1)
+            {
+                occurences++;
+                index = text.IndexOf(find, index + 1);
+            }
+
+            Console.WriteLine(occurences);
+        }
+    }
+}
---
+++ Technology Fundamentals/Strings and Text Processing- Lab/Palindromes/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindromes
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            char[] separators = new char[] { ' ', '.', '?', '!', ',' };
+            string[] words = Console.ReadLine().Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> palindromes = new List<string>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length == 1)
+                {
+                    palindromes.Add(words[i]);
+                }
+                else if (words[i].Length % 2 == 0)
+                {
+                    int middle = words[i].Length / 2;
+
+                    string left = words[i].Substring(0, middle);
+                    string right = words[i].Substring(middle);
+
+                    char[] rightChars = right.ToCharArray();
+
+                    string rightReverse = string.Empty;
+
+                    for (int j = right.Length - 1; j >= 0; j--)
+                    {
+                        rightReverse += rightChars[j];
+                    }
+
+                    if (left == rightReverse)
+                    {
+                        palindromes.Add(words[i]);
+                    }
+                }
+                else
+                {
+                    int middle = words[i].Length / 2;
+
+                    string left = words[i].Substring(0, middle + 1);
+                    string right = words[i].Substring(middle);
+
+                    char[] rightChars = right.ToCharArray();
+
+                    string rightReverse = string.Empty;
+
+                    for (int j = right.Length - 1; j >= 0; j--)
+                    {
+                        rightReverse += rightChars[j];
+                    }
+
+                    if (left == rightReverse)
+                    {
+                        palindromes.Add(words[i]);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", palindromes.Distinct().OrderBy(x => x)));
+        }
+    }
+}
