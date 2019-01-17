b71e4a37ac3280c8fc12eb5cfe1bc77bbc1eb331
PeterArnaudov <45295322+PeterArnaudov@users.noreply.github.com>
2019-01-18 00:28:31 +0200

    Add Solutions

---
+++ CSharp OOP/Defining Classes/DateModifier/DateModifier.cs
@@ -0,0 +1,18 @@
+﻿namespace DefiningClasses
+{
+    using System;
+    using System.Globalization;
+
+    public class DateModifier
+    {
+        public double Difference { get; set; }
+
+        public DateModifier(string dateOne, string dateTwo)
+        {
+            DateTime firstDate = DateTime.ParseExact(dateOne, "yyyy MM dd", CultureInfo.InvariantCulture);
+            DateTime secondDate = DateTime.ParseExact(dateTwo, "yyyy MM dd", CultureInfo.InvariantCulture);
+
+            Difference = Math.Abs((firstDate - secondDate).TotalDays);
+        }
+    }
+}
---
+++ CSharp OOP/Defining Classes/DateModifier/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace DefiningClasses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            DateModifier difference = new DateModifier(Console.ReadLine(), Console.ReadLine());
+            Console.WriteLine(difference.Difference);
+        }
+    }
+}
