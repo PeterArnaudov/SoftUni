765546d554d66dbd44293f9e00717d831f10acab
PeterArnaudov <45295322+PeterArnaudov@users.noreply.github.com>
2019-01-18 15:26:30 +0200

    Add Solutions

---
+++ CSharp OOP/Defining Classes/SpeedRacing/Car.cs
@@ -0,0 +1,38 @@
+﻿namespace SpeedRacing
+{
+    using System;
+
+    public class Car
+    {
+        public string Model { get; set; }
+
+        public double Fuel { get; set; }
+
+        public double Consumption { get; set; }
+
+        public double Traveled { get; set; }
+
+        public Car(string model, double fuel, double consumption)
+        {
+            Model = model;
+            Fuel = fuel;
+            Consumption = consumption;
+            Traveled = 0;
+        }
+
+        public void Drive(double distance)
+        {
+            double fuelNeeded = distance * Consumption;
+
+            if (Fuel >= fuelNeeded)
+            {
+                Fuel -= fuelNeeded;
+                Traveled += distance;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+    }
+}
---
+++ CSharp OOP/Defining Classes/SpeedRacing/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace SpeedRacing
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int numberOfCars = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                string[] info = Console.ReadLine().Split();
+
+                cars.Add(new Car(info[0], double.Parse(info[1]), double.Parse(info[2])));
+            }
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "End")
+                {
+                    break;
+                }
+
+                string[] commandArray = command.Split();
+
+                Car driven = cars.Find(x => x.Model == commandArray[1]);
+                driven.Drive(double.Parse(commandArray[2]));
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.Model} {car.Fuel:f2} {car.Traveled}");
+            }
+        }
+    }
+}
