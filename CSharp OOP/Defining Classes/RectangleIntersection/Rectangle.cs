bf48ffdb78e5beb6ee76dda9c459fd34e632b989
PeterArnaudov <45295322+PeterArnaudov@users.noreply.github.com>
2019-01-18 21:24:43 +0200

    Add Solutions

---
+++ CSharp OOP/Defining Classes/RectangleIntersection/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿namespace RectangleIntersection
+{
+    using System;
+
+    public class Rectangle
+    {
+        public string Id { get; set; }
+
+        public double Width { get; set; }
+
+        public double Height { get; set; }
+
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public Rectangle(string id, double width, double height, double x, double y)
+        {
+            Id = id;
+            Width = width;
+            Height = height;
+            X = x;
+            Y = y;
+        }
+
+        public bool DoRectanglesIntersect(Rectangle rectangle)
+        {
+            return rectangle.X + rectangle.Width >= this.X &&
+                rectangle.Y - rectangle.Height <= this.Y &&
+                rectangle.X <= this.X + this.Width &&
+                rectangle.Y >= this.Y - this.Height;
+        }
+    }
+}
---
+++ CSharp OOP/Defining Classes/RectangleIntersection/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace RectangleIntersection
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int[] loops = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            List<Rectangle> rectangles = new List<Rectangle>();
+
+            for (int i = 0; i < loops[0]; i++)
+            {
+                string[] info = Console.ReadLine().Split();
+
+                rectangles.Add(new Rectangle(info[0], double.Parse(info[1]), double.Parse(info[2]), double.Parse(info[3]), double.Parse(info[4])));
+            }
+
+            for (int i = 0; i < loops[1]; i++)
+            {
+                string[] info = Console.ReadLine().Split();
+
+                Rectangle rectangleOne = rectangles.Find(x => x.Id == info[0]);
+                Rectangle rectangleTwo = rectangles.Find(x => x.Id == info[1]);
+
+                if (rectangleOne.DoRectanglesIntersect(rectangleTwo))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
+        }
+    }
+}
