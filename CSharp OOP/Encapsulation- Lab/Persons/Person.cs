b5a17d2f4e28539136bc2b50fb25ced316b489d5
PeterArnaudov <45295322+PeterArnaudov@users.noreply.github.com>
2019-01-27 13:05:05 +0200

    Add Solutions

---
+++ CSharp OOP/Encapsulation- Lab/Persons/Person.cs
@@ -0,0 +1,27 @@
+﻿namespace PersonsInfo
+{
+    using System;
+
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public string FirstName { get; }
+
+        public int Age { get; }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.firstName} {this.lastName} is {this.age} years old.";
+        }
+    }
+}
---
+++ CSharp OOP/Encapsulation- Lab/Persons/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace PersonsInfo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] info = Console.ReadLine().Split();
+                people.Add(new Person(info[0], info[1], int.Parse(info[2])));
+            }
+
+            people.OrderBy(p => p.FirstName)
+                .ThenBy(p => p.Age)
+                .ToList()
+                .ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
