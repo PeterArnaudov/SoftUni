4e416142f09e07c840b27229615f491158f60ad9
PeterArnaudov <45295322+PeterArnaudov@users.noreply.github.com>
2019-01-30 00:30:10 +0200

    Solution Fix

--- CSharp Advanced/Functional Programming- Exercises/FindEvensOrOdds/FindEvensOrOdds.cs
+++ CSharp Advanced/Functional Programming- Exercises/FindEvensOrOdds/FindEvensOrOdds.cs
@@ -12,7 +12,7 @@
 
             Predicate<int> isEven = x => x % 2 == 0;
 
-            int[] filtered = Enumerable.Range(range[0], range[1] - range[0]).Where((x => type == "even" ? isEven(x) : !isEven(x))).ToArray();
+            int[] filtered = Enumerable.Range(range[0], range[1] - range[0] + 1).Where((x => type == "even" ? isEven(x) : !isEven(x))).ToArray();
 
             Console.WriteLine(string.Join(" ", filtered));
         }
