94a34258e6dfd36b749d373b173fe81c08e596ab
PeterArnaudov <45295322+PeterArnaudov@users.noreply.github.com>
2019-03-22 03:29:35 +0200

    Update Solution

--- JS Core/JS Essentials/Document Object Model- Lab/5. VisitedSites/solution.js
+++ JS Core/JS Essentials/Document Object Model- Lab/5. VisitedSites/solution.js
@@ -1,35 +1,15 @@
 function solve() {
+    function onclick() {
+        let textElement = this.parentNode.children[1];
+        let text = textElement.textContent;
+        let number = text.match(/\d+/g);
+        text = text.replace(number, (+number + 1));
+        textElement.textContent = text;
+    }
+
     let aTagElements = document.getElementsByTagName('a');
-    let spanTagElements = document.getElementsByTagName('span')
-    let visits = {'softuni': 1, 'google': 2, 'youtube': 3, 'wiki': 4, 'gmail': 5, 'stack': 6};
 
-    aTagElements[0].addEventListener('click', () => {
-        visits['softuni']++;
-        spanTagElements[0].textContent = `Visited: ${visits["softuni"]} times`;
-    })
-
-    aTagElements[1].addEventListener('click', () => {
-        visits['google']++;
-        spanTagElements[1].textContent = `Visited: ${visits['google']} times`;
-    })
-
-    aTagElements[2].addEventListener('click', () => {
-        visits['youtube']++;
-        spanTagElements[2].textContent = `Visited: ${visits['youtube']} times`;
-    })
-
-    aTagElements[3].addEventListener('click', () => {
-        visits['wiki']++;
-        spanTagElements[3].textContent = `Visited: ${visits['wiki']} times`;
-    })
-
-    aTagElements[4].addEventListener('click', () => {
-        visits['gmail']++;
-        spanTagElements[4].textContent = `Visited: ${visits['gmail']} times`;
-    })
-
-    aTagElements[5].addEventListener('click', () => {
-        visits['stack']++;
-        spanTagElements[5].textContent = `Visited: ${visits['stack']} times`;
-    })
+    for (let aTagElement of aTagElements) {
+        aTagElement.addEventListener('click', onclick);
+    }
 }
